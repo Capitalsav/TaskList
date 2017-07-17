60f4da8ef19888cd4c919d1b4fac1cf5166873b9
Anton <stor.anton.dev@gmail.com>
2017-07-17 15:32:57 +0300

    create class for sqlite_open_helper

---
+++ app/src/main/java/com/example/user1/testtaskmanager/TaskManagerDbHelper.java
@@ -0,0 +1,17 @@
+package com.example.user1.testtaskmanager;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class TaskManagerDbHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
