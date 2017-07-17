cf93803c1ce486ad38999d9fd6eec80e711dcd4c
Anton <stor.anton.dev@gmail.com>
2017-07-17 14:57:00 +0300

    made contract class for database

---
+++ app/src/main/java/com/example/user1/testtaskmanager/TaskManagerContract.java
@@ -0,0 +1,28 @@
+package com.example.user1.testtaskmanager;
+
+
+import android.provider.BaseColumns;
+
+public final class TaskManagerContract {
+
+    private TaskManagerContract() {
+
+    }
+
+    public static final class TaskInDb implements BaseColumns {
+        public final static String TABLE_NAME = "tasks";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_TASK_NAME = "name";
+        public final static String COLUMN_TASK_START_DATE = "start_date";
+        public final static String COLUMN_TASK_END_DATE = "end_date";
+    }
+
+    public static final class StageInDb implements BaseColumns {
+        public final static String TABLE_NAME = "stages";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_STAGE_NAME = "name";
+        public final static String COLUMN_STAGE_NUMBER = "number";
+    }
+}
