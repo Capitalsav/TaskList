b1a1004141703302899656ae44c3b70816b5094b
Anton <stor.anton.dev@gmail.com>
2017-07-18 09:48:12 +0300

    made save datePicker result to string in createTaskActivity

--- app/src/main/java/com/example/user1/testtaskmanager/CreateTaskActivity.java
+++ app/src/main/java/com/example/user1/testtaskmanager/CreateTaskActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 
@@ -24,8 +25,8 @@ public class CreateTaskActivity extends AppCompatActivity {
     private List<MyStage> myStageList;
 
     private String mTaskTitle;
-    private Calendar startDateCalendar;
-    private Calendar endDateCalendar;
+    private String startDateString;
+    private String endDateString;
 
     private Calendar currentDate;
 
@@ -53,16 +54,47 @@ public class CreateTaskActivity extends AppCompatActivity {
     }
 
     public void onClickStartDate(View view) {
-        new DatePickerDialog(CreateTaskActivity.this, startDateListener, currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DAY_OF_MONTH)).show();
+        new DatePickerDialog(CreateTaskActivity.this, startDateListener,
+                currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH),
+                currentDate.get(Calendar.DAY_OF_MONTH)).show();
     }
 
     public void onClickEndDate(View view) {
+        new DatePickerDialog(CreateTaskActivity.this, endDateListener,
+                currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH),
+                currentDate.get(Calendar.DAY_OF_MONTH)).show();
     }
 
     DatePickerDialog.OnDateSetListener startDateListener = new DatePickerDialog.OnDateSetListener() {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            if (view.isShown()) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(year);
+                builder.append("-");
+                builder.append(month);
+                builder.append("-");
+                builder.append(dayOfMonth);
+                startDateString = builder.toString();
+                Log.d(TAG, startDateString);
+            }
+        }
+    };
 
+    DatePickerDialog.OnDateSetListener endDateListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            if (view.isShown()) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(year);
+                builder.append("-");
+                builder.append(month);
+                builder.append("-");
+                builder.append(dayOfMonth);
+                endDateString = builder.toString();
+                Log.d(TAG, endDateString);
+            }
         }
     };
 }
