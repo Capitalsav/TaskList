719f04ae16426f516b468cd2f3a92de807f45604
Anton <stor.anton.dev@gmail.com>
2017-07-20 15:29:37 +0300

    get Tasks from DB without Calendars

--- app/src/main/java/com/example/user1/testtaskmanager/MainActivity.java
+++ app/src/main/java/com/example/user1/testtaskmanager/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.user1.testtaskmanager;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
-    private List<MyTask> mDataset = new ArrayList<>();
+    private ArrayList<MyTask> taskArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
         /*use linear layout manager*/
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
-        mAdapter = new RecyclerViewAdapter(mDataset);
+        mAdapter = new RecyclerViewAdapter(taskArrayList);
         mRecyclerView.setAdapter(mAdapter);
-        
+        selectAllTasks();
     }
 
 
@@ -48,5 +49,75 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    private void selectAllTasks() {
+        SQLiteDatabase database = mTaskManagerDbHelper.getReadableDatabase();
+        String[] selection = {
+                TaskManagerContract.TaskInDb._ID,
+                TaskManagerContract.TaskInDb.COLUMN_TASK_NAME,
+                TaskManagerContract.TaskInDb.COLUMN_TASK_START_DATE,
+                TaskManagerContract.TaskInDb.COLUMN_TASK_END_DATE
+        };
 
+        Cursor cursor = database.query(
+                TaskManagerContract.TaskInDb.TABLE_NAME,
+                selection,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        try{
+            int idColumnIndex = cursor.getColumnIndex(TaskManagerContract.TaskInDb._ID);
+            int nameColumnIndex = cursor.getColumnIndex(TaskManagerContract.TaskInDb.COLUMN_TASK_NAME);
+            int startDateColumnIndex = cursor.getColumnIndex(TaskManagerContract.TaskInDb.COLUMN_TASK_START_DATE);
+            int endDateColumnIndex = cursor.getColumnIndex(TaskManagerContract.TaskInDb.COLUMN_TASK_END_DATE);
+
+            while (cursor.moveToNext()) {
+                int currentId = cursor.getInt(idColumnIndex);
+                String taskName = cursor.getString(nameColumnIndex);
+                ArrayList<MyStage> stageArrayList = new ArrayList<>();
+
+                String [] selectionStage = {
+                        TaskManagerContract.StageInDb.COLUMN_STAGE_NAME,
+                        TaskManagerContract.StageInDb.COLUMN_STAGE_NUMBER,
+                        TaskManagerContract.StageInDb.COLUMN_STAGE_TASK_ID
+                };
+
+                String selectionWhere = TaskManagerContract.StageInDb.COLUMN_STAGE_TASK_ID + "=?";
+                String[] selectionWhereArgs = {String.valueOf(currentId)};
+
+                Cursor cursorStage = database.query(
+                        TaskManagerContract.StageInDb.TABLE_NAME,
+                        selectionStage,
+                        selectionWhere,
+                        selectionWhereArgs,
+                        null,
+                        null,
+                        null
+                );
+                int idStageColumnIndex = cursorStage.getColumnIndex(TaskManagerContract.StageInDb._ID);
+                int nameStageColumnIndex = cursorStage.getColumnIndex(TaskManagerContract.StageInDb.COLUMN_STAGE_NAME);
+                int numnberStageColumnIndex = cursorStage.getColumnIndex(TaskManagerContract.StageInDb.COLUMN_STAGE_NUMBER);
+
+                while (cursorStage.moveToNext()){
+                    MyStage myStage = new MyStage();
+                    myStage.setmStageName(cursorStage.getString(nameStageColumnIndex));
+                    stageArrayList.add(myStage);
+                }
+                MyTask myTask = new MyTask();
+                myTask.setmTaskName(taskName);
+                myTask.setMyStages(stageArrayList);
+                taskArrayList.add(myTask);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            cursor.close();
+            database.close();
+        }
+    }
 }
